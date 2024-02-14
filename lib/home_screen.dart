5236ec3cdd1af192d2e4a1b823e0a1bb69232af3
DiegoARomeroH <diegoalonsoromerohernndez@gmail.com>
2024-02-14 13:52:10 -0600

    Adding listitle to the Listview

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,38 +7,33 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Componentes de flutter'),
+        title: const Text('Componentes de flutter', style: TextStyle(color: Colors.cyan),), 
       ),
         body: ListView(
           children: const [
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
-            Text('KC campeones de la SB'),
+            ListTile(
+              leading: Icon(Icons.boy),
+              title: Text('KC campeones de la SB',
+                     style: TextStyle(
+                     color: Colors.amber, 
+                     fontWeight: FontWeight.bold),),
+              subtitle: Text('11-02-2024 - Las vegas'),
+              trailing: Icon(Icons.arrow_circle_right),
+            ),
+            Divider(),
+            ListTile(
+              leading:  Icon(Icons.bolt),
+              title: Text('KC campeones de la SB'),
+              subtitle: Text('10-02-2023 - Arizona'),
+              trailing: Icon(Icons.arrow_circle_right),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.ac_unit),
+              title: Text('KC campeones de la SB'),
+              subtitle: Text('10-02-2023 - California'),
+              trailing: Icon(Icons.arrow_circle_right),
+            ),
           ],
         ) 
       );
