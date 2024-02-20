13cdb052bbd8ab309d22ec64751a710954bd20e1
DiegoARomeroH <diegoalonsoromerohernndez@gmail.com>
2024-02-20 13:52:51 -0600

    Add inputs, infinite_list and notification screen

---
+++ lib/screens/inputs_screens.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Inputscreen extends StatefulWidget {
+  const Inputscreen({super.key});
+
+  @override
+  State<Inputscreen> createState() => _InputscreenState();
+}
+
+class _InputscreenState extends State<Inputscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Inputs'),
+    );
+  }
+}
